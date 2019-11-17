4f0c93a64096228ea9f5ec37e2d0fc975ef01328
Takaaki Uno <takaaki.uno@gmail.com>
2019-11-18 02:09:24 +0900

    modify release_pathからcurrent_pathへ変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -81,7 +81,8 @@ namespace :deploy do
   task :db_create do
     on roles(:db) do
       with rails_env: fetch(:rails_env) do
-        within release_path do
+        # within release_path do
+        within current_path do
           execute :bundle, :exec, :rake, 'db:create'
         end
       end
