bb456071f47646176802c321b3eae4fd58a9f9fe
Takaaki Uno <takaaki.uno@gmail.com>
2020-01-13 02:37:43 +0900

    add update_tweet_datetimeメソッドを追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -18,4 +18,20 @@ class Task < ApplicationRecord
       active!
     end
   end
+
+  def update_tweet_datetime
+    if tweet_datetime < Time.current
+      # 指定日時・時刻を文字列で取得
+      today = Time.current.to_date.to_s
+      tweet_time = tweet_time.to_s.split[1]
+      today_tweet_datetime = (today + ' ' + tweet_time).in_time_zone
+      if every_day?
+        tweet_datetime = (Date.tomorrow.to_s + ' ' + tweet_time).in_time_zone
+      elsif task.every_week?
+        # 次の指定曜日の日付を取得
+        next_wday = Time.current.beginning_of_week(task.tweet_dayofweek.to_sym).since(1.week)
+        task.tweet_datetime = (next_wday.to_date.to_s + ' ' + tweet_time).in_time_zone
+      end
+    end
+  end
 end
